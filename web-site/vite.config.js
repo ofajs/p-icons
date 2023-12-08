59a0ef85d2868187c19854ccf74c4ebece46e1b5
machao <frmachao@126.com>
2023-12-08 17:35:01 +0800

    chore: config vite build

--- web-site/vite.config.js
+++ web-site/vite.config.js
@@ -1,12 +1,21 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '../docs',
-    emptyOutDir:true,
+    outDir: "../docs",
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+      },
+    },
   },
-  base:'/p-icons/'
+  base: "/p-icons/",
 });
